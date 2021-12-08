0c6d322541204f6139edd64c3d7b847de5623936
Aoi <ht.so.ik0102@gmail.com>
2021-12-08 07:18:59 +0700

    create project

--- test/crowdFunding.ts
+++ test/crowdFunding.ts
@@ -1,6 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { CrowdFunding } from "../target/types/crowd_funding";
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
 
 describe("CrowdFunding", () => {
   // Configure the client to use the local cluster.
@@ -9,7 +16,15 @@ describe("CrowdFunding", () => {
   const program: anchor.Program = anchor.workspace.CrowdFunding;
   const provider = anchor.Provider.env();
   const wallet = provider.wallet as anchor.Wallet;
-  
+
+  interface Project {
+    project_id: number;
+    representative: PublicKey;
+    current_amount: number;
+    goal_amount: number;
+    deadline: number;
+    achieved: boolean;
+  }
 
   it("Is initialized!", async () => {
     // Add your test here.
